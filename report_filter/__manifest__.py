168a2099c96c8103c59b816228e0bbbc5c3b3dce
spellboundodoo <contact@spellboundss.com>
2017-08-23 16:43:03 +0530

    company name change

--- report_filter/__manifest__.py
+++ report_filter/__manifest__.py
@@ -8,7 +8,7 @@ Filter Partner and Account ON General Ledger Report
 ====================
     """,
     'category': 'Account',
-    'author' : 'Spellbound Soft Solution',
+    'author' : 'Spellbound Soft Solutions',
     'website': 'https://www.spellboundss.com',
     'depends' : ['base', 'account', 'report',],
     'data': [
